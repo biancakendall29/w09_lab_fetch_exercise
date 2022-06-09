a7c87541e07150e02deb9ce486ff7590d14c5976
Bianca Kendall <[D)>
2022-06-09 17:20:04 +0200

    implemented getAllCountries function

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-const getCountryByName = (countryName) => {
+const getCountryByName = (countryName, item) => {
+    item = addParagraph();
     return new Promise((resolve, reject) => {
         fetch(`https://restcountries.com/v2/name/${countryName}`)
         .then(response => response.json())
         // .then(data => console.log(data[0]))
         .then((data) => {
-            paragraphName.innerHTML = `Country name is ${data[0].name} and population is ${data[0].population}`;
+            item.innerHTML = `Country name is ${data[0].name} and population is ${data[0].population}`;
+            appendToSection(item);
             resolve();
         })
     })
-    
+}
+
+const addParagraph = () => {
+    return item = document.createElement('p');
+}
+
+const appendToSection = (item) => {
+    sect.append(item);
+}
+
+function getAllCountries() {
+    return new Promise((reolve, reject) => {
+        fetch("https://restcountries.com/v2/all")
+        .then(response => response.json())
+        .then((data) => {
+            for (let i = 0; i < 250; i++) {
+            para = addParagraph();
+            para.innerHTML = `Country name is ${data[i].name} and population is ${data[i].population}`
+            appendToSection(para)
+            }
+            resolve();
+        })
+    })
 }
 
 
 const sect = document.getElementById("section");
 sect.innerHTML = "";
-const paragraphName = document.createElement('p');
-const paragraphPop = document.createElement('p');
-getCountryByName("cuba");
 
+const item1 = null;
+const item2 = null;
+getCountryByName("cuba", item1);
+getCountryByName("mozam", item2);
 
-sect.append(paragraphName); sect.append(paragraphPop);
+getAllCountries();
 
 
 
@@ -27,3 +52,5 @@ sect.append(paragraphName); sect.append(paragraphPop);
 // Modify your code to additionally display the population of the specified country on your webpage
 // Create a getAllCountries() function which similarly displays the name and population of each country to the frontend. See the tip below about abstracting your functionality. Have this method called on page load
 // Create an HTML form which allows you to input the name of a country which is used to "filter" the information on the front-end on-submit. This will call your getCountryByName() function
+
+// When you create JS functionality for adding your object properties to, for instance, a p element, it would make sense to create this as a separate function (abstracted function) which accepts the data of one country. This is so you can easily reuse this functionality during the later tasks 
